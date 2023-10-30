1b5c6c20afe6fda4e20933d72349a521191d9c10
GN-zll <minkal2sh@gmail.com>
2023-10-30 14:38:04 +0300

    Do Task3

--- Homework4/Task5/Test.java
+++ Homework4/Task5/Test.java
@@ -3,10 +3,10 @@ package Homework4.Task5;
 public class Test {
   static boolean singUp (String login, String password, String confirmPassword) {
     try {
-    if(!login.matches("^[a-zA-Z0-9_]+$") || login.length()>10) {
+    if(!login.matches("^[a-zA-Z0-9_]+$") || login.length()>=20) {
         throw new WrongLoginException();
     }
-    if(!password.matches("^[a-zA-Z0-9_]+$") || password.length()>10 || password.equals(confirmPassword)){
+    if(!password.matches("^[a-zA-Z0-9_]+$") || password.length()>=20 || password.equals(confirmPassword)){
       throw new WrongPasswordException();
     }
     return true;
