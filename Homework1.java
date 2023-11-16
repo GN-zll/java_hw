66acafb77a0f051f0c3502dc5e6225e14efd586e
GN-zll <minkal2sh@gmail.com>
2023-11-16 13:09:49 +0300

    corrected average

--- Homework1.java
+++ Homework1.java
@@ -108,8 +108,8 @@ public class Homework1 {
             return null;
         }
         int sum = 0;
-        for (int i = 0; i < nums.length; ++i) {
-            sum += nums[i];
+        for (int i : nums) {
+            sum += i;
         }
         return (double) sum/nums.length;
     }
